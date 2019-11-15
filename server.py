893a125c32a831dee864f009980c7e91f1ca536f
Jonathan T. Risoldi <jr547@cairn.edu>
2019-11-15 00:33:06 -0500

    Working on the loop blocking issue.

--- server.py
+++ server.py
@@ -1,12 +1,13 @@
 import socket
 import sys
 import time
+import msvcrt
 
 
 s = socket.socket()
 host = socket.gethostname()
 print("Server will start on host:", host)
-port = 8230
+port = 25000
 s.bind((host,port))
 print("")
 print("Server done binding to host and port successfully")
@@ -18,11 +19,25 @@ conn, addr = s.accept()
 print(addr," Has connected to the server and is now online...")
 print("")
 
-while 1:
-    message = input(str(">>"))
-    message = message.encode()
-    conn.send(message)
+#num = 0
+#done = False
+#while not done:
+#    print(num)
+#    num += 1
 
+#    if msvcrt.kbhit():
+#        print (msvcrt.getch())
+#        done = True
+while 1:
+    if msvcrt.kbhit():
+        m = msvcrt.getch()
+        if m == 13:
+            message = m
+            message = message.encode()
+            conn.send(message)
+            message = ""
+        print (m)
+        
     incoming_message = conn.recv(1024)
     incoming_message = incoming_message.decode()
     print("Client: ",incoming_message)
