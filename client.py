4b293d7854c0e9e243fad01b427c2a6ce5fdd26c
Jonathan T. Risoldi <jr547@cairn.edu>
2019-11-15 00:31:20 -0500

    Changed Port.

--- client.py
+++ client.py
@@ -4,9 +4,10 @@ import time
 
 s = socket.socket()
 host = input(str("Please enter the hostname of the server:"))
-port = 8230
+port = 25000
 s.connect((host,port))
 print("Connected to chat server")
+
 while 1:
     incoming_message = s.recv(1024)
     incoming_message = incoming_message.decode()
