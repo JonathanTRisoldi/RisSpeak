68c274ded26afe1fc85814a55702bceb07fd0859
Jonathan T. Risoldi <32909097+JonathanTRisoldi@users.noreply.github.com>
2019-12-03 14:07:54 -0500

    Key commands, sending format

--- client.py
+++ client.py
@@ -5,22 +5,28 @@ import msvcrt
 
 s = socket.socket()
 host = input(str("Please enter the hostname of the server:"))
-port = 25000
+port = 25100
 s.connect((host,port))
 print("Connected to chat server")
 
+message = ""
 while 1:
     if msvcrt.kbhit():
-        m = msvcrt.getch().decode('utf-8')
-        if m == "x":
-            #print("Enter has been hit")
-             message = m
-             message = message.encode()
-             conn.send(message)
-             message = ""
+        m = msvcrt.getch().decode('ASCII')
+        message += m
+        if m == '\r':
+            #print('FREAKING WORK')
+            message = message.encode()
+            s.send(message)
+            message = ""
+            print("")
+        elif m == '\b':
+            print('\b', end="", flush=True)
+            print(' ', end="", flush=True)
+            print('\b', end="", flush=True)
         else:
             print (m, end="", flush=True)
     
-    incoming_message = s.recv(1024)
-    incoming_message = incoming_message.decode()
-    print("Server: ",incoming_message)
+    #incoming_message = s.recv(1024)
+    #incoming_message = incoming_message.decode()
+    #print("Server: ",incoming_message)
