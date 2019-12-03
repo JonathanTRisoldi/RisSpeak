4d420f26bb9814b12b9c84d359a25ffc25b3bc48
Jonathan T. Risoldi <32909097+JonathanTRisoldi@users.noreply.github.com>
2019-12-03 14:48:07 -0500

    Fixed minor formating issue.

--- cr.py
+++ cr.py
@@ -8,9 +8,9 @@ host = input(str("Please enter the hostname of the server:"))
 port = 25100
 s.connect((host,port))
 print("Connected to chat server")
-crid = input(str("Please enter a reciver ID:"))
+crid = input(str("Please create a reciver ID:"))
 
 while 1:  
     incoming_message = s.recv(1024)
     incoming_message = incoming_message.decode()
-    print("Server: ",incoming_message)
+    print("Sender: ",incoming_message)
