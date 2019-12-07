6d9af447e003d2cbfc8e00da45b6d97a7988ac90
Ris <jrisoldi@risoldi.tech>
2019-12-07 04:22:30 -0500

    Complete server revamp

--- server.py
+++ server.py
@@ -1,53 +1,69 @@
 import socket
-import sys
-import time
-import msvcrt
-#import threading
+import select
 
-# Order of operations:
-#   1)Server starts
-#   2)CR connects
-#   3)server re-assigns CR to new socket
-#   4)CS connects
-#   5)Server re-assigns CS to appropriate socket bassed off of crid 
+HEADER_LENGTH = 10
+IP = "127.0.0.1"
+PORT = 1234
 
-s = socket.socket()
-host = socket.gethostname()
-print("Server will start on host:", host)
-port = 25100
-s.bind((host,port))
-print("")
-print("Server done binding to host and port successfully")
-print ("")
-print("Server is waiting for incomming connections")
-print("")
-s.listen(1)
-conn, addr = s.accept()
-print(addr," Has connected to the server and is now online...")
-print("")
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-message = ""
+server_socket.bind((IP, PORT))
 
-while 1:
-    if msvcrt.kbhit():
-        m = msvcrt.getch().decode('ASCII')
-        message += m
-        if m == '\r':
-            #print('FREAKING WORK')
-            message = message.encode()
-            conn.send(message)
-            message = ""
-            print("")
-        elif m == '\b':
-            print('\b', end="", flush=True)
-            print(' ', end="", flush=True)
-            print('\b', end="", flush=True)
+server_socket.listen()
+
+sockets_list = [server_socket]
+
+clients = {}
+
+
+
+def recive_message(client_socket):
+    try:
+        message_header = client_socket.recv(HEADER_LENGTH)
+
+        if not len(message_header):
+            return False
+        
+        message_length = int(message_header.decode("utf-8").strip())
+        return {"header": message_header, "data": client_socket.recv(message_length)}
+
+    except:
+        return False
+
+while True:
+    read_sockets, _, exception_sockets = select.select(sockets_list, [], sockets_list)
+
+    for notified_socket in read_sockets:
+        if notified_socket == server_socket:
+            client_socket, client_address = server_socket.accept()
+
+            user = recive_message(client_socket)
+            if user is False:
+                continue
+
+            sockets_list.append(client_socket)
+
+            clients[client_socket] = user
+
+            print(f"Accepted new connection from {client_address[0]}:{client_address[1]} username:{user['data'].decode('utf-8')}")
+
         else:
-            print (m, end="", flush=True)
-#def recv():
-#    while True:
-#        incoming_message = conn.recv(1024)
-#        incoming_message = incoming_message.decode()
-#        print("Client: ",incoming_message)
-#        if not incoming_message: continue
-#threading.Thread(group=None, target=recv, name=None, args=(), kwargs=None, daemon=None).start()
+            message = recive_message(notified_socket)
+
+            if message is False:
+                print(f"Closed connection from {clients[notified_socket]['data'].decode('utf-8')}")
+                sockets_list.remove(notified_socket)
+                del clients[notified_socket]
+                continue
+
+            user = clients[notified_socket]
+
+            print(f"{user['data'].decode('utf-8')}: {message['data'].decode('utf-8')}")
+
+            for client_socket in clients:
+                if client_socket != notified_socket:
+                    client_socket.send(user['header'] + user['data'] + message['header'] + message['data'])
+    for notified_socket in exception_sockets:
+        sockets_list.remove(notified_socket)
+        del clients[notified_socket]
